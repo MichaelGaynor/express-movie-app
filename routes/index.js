4a622f58555bc3eae262437cd08e8c37c2d57cd1
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-06-01 14:45:17 -0400

    fun changes and wildcard url

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,45 @@ var router = express.Router();
 var request = require('request');
 var config = require('../config/config');
 
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-    const apiBaseUrl = 'http://api.themoviedb.org/3';
-    const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey
-    const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
-
     request.get(nowPlayingUrl,(error,response,movieData)=>{
         var movieData = JSON.parse(movieData);
         res.render('index', { 
             movieData: movieData.results,
-            imageBaseUrl: imageBaseUrl
+            imageBaseUrl: imageBaseUrl,
+            message: "Welcome to my movie app. These are now playing."
          });
     })
+});
 
+router.post('/search', (req, res)=>{
+    var termUserSearchedFor = req.body.searchString;
+    var searchUrl = apiBaseUrl + '/search/movie?query='+termUserSearchedFor+'&api_key='+config.apiKey;
+    request.get(searchUrl,(error,response,movieData)=>{
+        var movieData = (JSON.parse(movieData));
+        res.render('index', {
+            movieData: movieData.results,
+            imageBaseUrl: imageBaseUrl,
+            message: "Results for "+ termUserSearchedFor +":"
+        });
+    });
+});
+
+router.get('/movie/:id', (req, res)=>{
+    var thisMovieId = req.params.id
+    var thisMovieUrl = apiBaseUrl + '/movie/'+thisMovieId+'?api_key='+config.apiKey;
+    request.get(thisMovieUrl, (error,response,movieData)=>{
+        var movieData = JSON.parse(movieData);
+        res.render('single-movie', {
+            movieData: movieData,
+            imageBaseUrl: imageBaseUrl
+        });
+    });
 });
 
 module.exports = router;
